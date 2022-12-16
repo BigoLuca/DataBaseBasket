e97f2ba97cb36de9391ef93f8db3fbe53886dcc1
LucaBighini <bighini.luca@gmail.com>
2022-12-16 13:35:17 +0100

    update query operazioni

--- ER/Query.sql
+++ ER/Query.sql
@@ -1,98 +1,114 @@
 -- OP1 Inserire allenatore
 INSERT INTO ALLENATORE (CF, nome, cognome, eta, telefono, email) 
-VALUES (?, ?, ?, ?, ?, ?)
+VALUES (?, ?, ?, ?, ?, ?);
 
 -- OP2 Inserire giocatore assegnandogli una squadra
 INSERT INTO GIOCATORE (CF, nome, cognome, eta, telefono, email, nMaglia)
-VALUES (?, ?, ?, ?, ?, ?,?)
+VALUES (?, ?, ?, ?, ?, ?,?);
 INSERT INTO appartiene (idGiocatore, idSquadra)
-VALUES (?, ?)
+VALUES (?, ?);
 UPDATE SQUADRA
-SET nGiocatori = nGiocatori + 1
+SET nGiocatori = nGiocatori + 1;
 
 -- OP3 Visualizzare il numero totale di persone della società
-SELECT COUNT (*) AS persone
-FROM ALLENATORE, GIOCATORE
+SELECT COUNT(*) AS persone
+FROM ALLENATORE, GIOCATORE;
 
 -- OP4 Creare una squadra
 INSERT INTO SQUADRA (idSquadra, nome, annata, nGiocatori)
-VALUES (?, ?, ?, ?)
+VALUES (?, ?, ?, ?);
 
 -- OP5 Modificare annata di una squadra
 UPDATE SQUADRA
-SET annata = ?
+SET annata = ?;
 
 -- OP6 Rimuovere un giocatore da una squadra
 DELETE FROM appartiene
-WHERE GIOCATORE.idGiocatore = ?
+WHERE GIOCATORE.idGiocatore = ?;
 UPDATE SQUADRA
-WHERE SQUADRA.idSquadra = ?
 SET nGiocatori = nGiocatori - 1
+WHERE SQUADRA.idSquadra = ?;
 
 -- OP7 Assegnare un allenatore ad una squadra
 INSERT INTO gestisce (idAllenatore, idSquadra)
-VALUES (?, ?)
+VALUES(?, ?);
 
 -- OP8 Visualizzare i giocatori di una squadra
 SELECT GIOCATORE
 FROM appartiene A, GIOCATORE G
-WHERE A.idGiocatore = G.idGiocatore AND A.idSquadra = ?
+WHERE A.idGiocatore = G.idGiocatore AND A.idSquadra = ?;
 
 -- OP9 Assegnare un giocatore ad un campo estivo
 INSERT INTO partecipa (idGiocatore, idCampoEstivo)
-VALUES (?, ?)
+VALUES (?, ?);
 
 -- OP10 Assegnare un allenatore ad un campo estivo
 INSERT INTO organizza (idAllenatore, idCampoEstivo)
-VALUES (?, ?)
+VALUES (?, ?);
 
 -- OP11 Visualizzare i giocatori di un campo estivo
 SELECT GIOCATORE
 FROM partecipa P
-WHERE P.idCampoEstivo = ?
+WHERE P.idCampoEstivo = ?;
 
 -- OP12 Visualizzare le attrezzature di una palestra
 SELECT ATTREZZATURE, dispone.quantita
 FROM dispone D
-WHERE D.idPalestra = ?
+WHERE D.idPalestra = ?;
 
 -- OP13 Visualizzare le partite di una squadra
 SELECT PARTITA
 FROM PARTITA P
-WHERE P.idSquadra = ?
+WHERE P.idSquadra = ?;
 
-OP-14 Visualizzare il campionato di ogni squadra
-SELECT CAMPIONATO, SQUADRA.idSquadra
-FROM iscritta I
-WHERE iscritta.anno = ?
-OP-15 Visualizzare gli allenamenti di una squadra
-SELECT ALLENAMENTO
-WHERE pratica.idSquadra = ?
-OP-16 Acquistare x paia di calzini
-Innanzitutto, bisogna controllare che gli oggetti desiderati siano disponibili. 
+-- OP14 Visualizzare i campionato giocati da una squadra negli anni (in ordine decrescente)
+SELECT PARTITA.campionato_anno, PARTITA.campionato_nome, PARTITA.campionato_livello 
+FROM PARTITA P
+WHERE P.idSquadra = ?
+ORDER BY P.campionato_anno DESC;
+
+-- OP15 Visualizzare gli allenamenti di una squadra e in che palestra
+SELECT ALLENAMENTO, PALESTRA
+FROM ALLENAMENTO A, PALESTRA P
+WHERE A.idPalestra = P.idPalestra AND A.idSquadra = ?;
+
+-- OP-16 Acquistare x paia di calzini
+INSERT INTO richiesta(idMateriale, idVendita, quantita)
+SELECT MATERIALE.idMateriale, VENDITA.idVendita, VALUES(?)
+WHERE MATERIALE.disponibilita > ?;
+-- Innanzitutto, bisogna controllare che gli oggetti desiderati siano disponibili. 
 SELECT disponibilita 
 FROM MATERIALE
 WHERE idMateriale = ? 
-AND disponibilita >= ?
-Una volta controllato che l’oggetto sia disponibile, procedo all’acquisto aggiungendo una nuova vendita. 
-INSERT INTO acquisto (idGiocatore, idMateriale, quantita) 
-VALUES (?, ?, ?)
-Creo un nuovo scontrino
-INSERT INTO SCONTRINO (idScontrino, data, ora, valore, sconto) 
-VALUES (?, GETDATE(), ?, ?, ?)
-A questo punto bisogna diminuire la quantità dell’oggetto. 
+AND disponibilita >= ?;
+-- Creo un nuovo scontrino
+INSERT INTO VENDITA (idVendita, data, costo_totale) 
+VALUES (?, GETDATE(), MATERIALE.prezzo * richiesta.quantita);
+-- A questo punto bisogna diminuire la quantità dell’oggetto. 
 UPDATE MATERIALE
 SET quantita = quantita - ? 
-WHERE idMateriale = ?
-OP-17 Visualizzare i materiali disponibili
-SELECT MATERIALI 
-OP-18 Visualizzare il guadagno totale
-SELECT SUM(valore) AS guadagno
-FROM SCONTRINO S 
-I dati da inserire sono la data iniziale e la data finale fra cui visualizzare i guadagni (estremi inclusi), la costante 105 indica il formato italiano per DateTime (gg-mm-aaaa) 
-WHERE S.data BETWEEN CONVERT(DATETIME, ?, 105) AND CONVERT(DATETIME, ?, 105)+1 
-Nel caso si voglia inserire solo l’anno
-WHERE DATEPART(yyyy, P.data) = ?
-OP-19 Visualizzare numero di giocatori in una squadra
-SELECT SQUADRA.nGiocatori
-WHERE  SQUADRA.idSquadra = ?
+WHERE idMateriale = ?;
+
+-- OP-17 Visualizzare i materiali disponibili
+SELECT MATERIALI;
+
+-- OP18 Visualizzare numero di vendite fatte nelle varie sedi
+SELECT SEDE, COUNT(*) AS N_VENDITE
+FROM VENDITA V
+WHERE V.idSede = S.idSede
+GROUP BY SEDE.idSede;
+
+-- OP19 Visualizzare il guadagno totale per anno di una sede
+SELECT SUM(Q.costo) AS Quote, (	SELECT SUM(V.costo_totale)
+								FROM VENDITE V
+                                WHERE V.idSede = ? AND DATEPART(yyyy, V.data) = ? ) AS Vendite
+FROM QUOTA Q
+WHERE QUOTA.idGiocatore = (	SELECT GIOCATORE.idGipocatore
+							FROM GIOCATORE
+							WHERE GIOCATORE.idSede = ?)
+AND Q.anno = ?;
+
+
+-- OP-20 Visualizzare numero di giocatori in ogni squadra
+SELECT SQUARA, COUNT(SQUADRA.nGiocatori) AS Giocatori
+GROUP BY SQUADRA.idSquadra;
